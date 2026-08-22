a556e1251636a62099a417e2a76cbf6403aef081
drbelalgenedy <zahershabaanabd@gmail.com>
2026-08-22 12:37:07 +0300

    v13 - live sync between devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='bg-accounts-v122';
+const C='bg-accounts-v124';
 const ASSETS=['./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html'].concat(ASSETS))).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
